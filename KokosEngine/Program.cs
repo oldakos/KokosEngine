f1f0bcca5856f3d0381ac57e2447633fc05c9732
oldakos <oldakos@volny.cz>
2023-06-14 10:36:34 +0200

    Add project files.

---
+++ KokosEngine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KokosEngine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
