867c5fa8ed1dbb8f219bd426982ae7c61d935d8a
oldakos <oldakos@volny.cz>
2023-06-14 16:10:42 +0200

    Create Board class

---
+++ KokosEngine/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KokosEngine
+{
+    internal class Board
+    {
+        ulong whitePawns;
+        ulong whiteKnights;
+        ulong whiteBishops;
+        ulong whiteRooks;
+        ulong whiteQueens;
+        ulong whiteKing;
+
+        ulong blackPawns;
+        ulong blackKnights;
+        ulong blackBishops;
+        ulong blackRooks;
+        ulong blackQueens;
+        ulong blackKing;
+    }
+}
