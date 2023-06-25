ec2b79ed8c4ef34a0a5d5d0810eaf00f3867ee1e
oldakos <oldakos@volny.cz>
2023-06-25 10:02:17 +0200

    move weird methods from Board to new class BBUtil

--- KokosEngine/Board.cs
+++ KokosEngine/Board.cs
@@ -138,7 +138,7 @@ namespace KokosEngine
 
             sb.Append(' '); //section: en passant
 
-            sb.Append(LSB1ToAlgebraic(enPassant));
+            sb.Append(BBUtil.LSB1ToAlgebraic(enPassant));
 
             sb.Append(' '); //section: halfmove clock
 
@@ -149,46 +149,6 @@ namespace KokosEngine
             sb.Append(fullmovesTotal.ToString());
 
             return sb.ToString();
-        }
-        int bitScanForward(ulong bb)
-        {
-            if (bb == 0) throw new ArgumentException("you bitscanned a zero bitboard");
-            const ulong debruijn64 = 0x03f79d71b4cb0a89;
-            ulong negative = (ulong)(-(long)bb);
-            return index64[((bb & negative) * debruijn64) >> 58];
-        }
-        readonly int[] index64 = {
-             0,  1, 48,  2, 57, 49, 28,  3,
-            61, 58, 50, 42, 38, 29, 17,  4,
-            62, 55, 59, 36, 53, 51, 43, 22,
-            45, 39, 33, 30, 24, 18, 12,  5,
-            63, 47, 56, 27, 60, 41, 37, 16,
-            54, 35, 52, 21, 44, 32, 23, 11,
-            46, 26, 40, 15, 34, 20, 31, 10,
-            25, 14, 19,  9, 13,  8,  7,  6
-        };
-
-        /// <summary>
-        /// Return algebraic notation of the LSB1 square in given bitboard.
-        /// </summary>
-        /// <returns>Algebraic notation of a square, or "-" if bitboard is empty.</returns>
-        string LSB1ToAlgebraic(ulong bitboard)
-        {
-            if (bitboard == 0)
-            {
-                return "-";
-            }
-            int index = bitScanForward(bitboard);
-            return IndexToAlgebraic(index);
-        }
-        string IndexToAlgebraic(int index)
-        {
-            if (index < 0 || index > 63) throw new ArgumentException("you used an index outside 0-63");
-            int rankindex = index / 8;
-            int fileindex = index % 8;
-            char rank = (char)('1' + rankindex);
-            char file = (char)('a' + fileindex);
-            return new string(new char[] { file, rank });
         }
     }
 }
