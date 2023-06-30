31bff8afbb3fba0a533777a7b05a5c22278d4588
oldakos <oldakos@volny.cz>
2023-06-30 22:00:43 +0200

    finished MakeMove(move) ? needs testing

--- KokosEngine/Board.cs
+++ KokosEngine/Board.cs
@@ -9,74 +9,79 @@ namespace KokosEngine
 {
     internal class Board
     {
-        bool isWhiteToMove;
+        int PlayerToMove; //white=1 black=-1
+        bool WhiteMove
+        {
+            get { return PlayerToMove == 1; }
+            set { PlayerToMove = value ? 1 : -1; }
+        }
 
-        int halfmovesReversible; //halfmoves played since last capture or pawn move
-        int fullmovesTotal; //increments after black moves
+        int HalfmovesReversible; //halfmoves played since last capture or pawn move
+        int FullmovesTotal; //increments after black moves
 
-        ulong possibleEPSquare; //bitboard with 0 or 1 squares where en passant is possible
+        ulong PossibleEPSquare; //bitboard with 0 or 1 squares where en passant is possible
 
-        ulong[] bitboards;
+        ulong[] Bitboards;
         #region Bitboard Properties
         ulong WhitePawns
         {
-            get { return bitboards[0]; }
-            set { bitboards[0] = value; }
+            get { return Bitboards[0]; }
+            set { Bitboards[0] = value; }
         }
         ulong WhiteKnights
         {
-            get { return bitboards[1]; }
-            set { bitboards[1] = value; }
+            get { return Bitboards[1]; }
+            set { Bitboards[1] = value; }
         }
         ulong WhiteBishops
         {
-            get { return bitboards[2]; }
-            set { bitboards[2] = value; }
+            get { return Bitboards[2]; }
+            set { Bitboards[2] = value; }
         }
         ulong WhiteRooks
         {
-            get { return bitboards[3]; }
-            set { bitboards[3] = value; }
+            get { return Bitboards[3]; }
+            set { Bitboards[3] = value; }
         }
         ulong WhiteQueens
         {
-            get { return bitboards[4]; }
-            set { bitboards[4] = value; }
+            get { return Bitboards[4]; }
+            set { Bitboards[4] = value; }
         }
         ulong WhiteKing
         {
-            get { return bitboards[5]; }
-            set { bitboards[5] = value; }
+            get { return Bitboards[5]; }
+            set { Bitboards[5] = value; }
         }
         ulong BlackPawns
         {
-            get { return bitboards[6]; }
-            set { bitboards[6] = value; }
+            get { return Bitboards[6]; }
+            set { Bitboards[6] = value; }
         }
         ulong BlackKnights
         {
-            get { return bitboards[7]; }
-            set { bitboards[7] = value; }
+            get { return Bitboards[7]; }
+            set { Bitboards[7] = value; }
         }
         ulong BlackBishops
         {
-            get { return bitboards[8]; }
-            set { bitboards[8] = value; }
+            get { return Bitboards[8]; }
+            set { Bitboards[8] = value; }
         }
         ulong BlackRooks
         {
-            get { return bitboards[9]; }
-            set { bitboards[9] = value; }
+            get { return Bitboards[9]; }
+            set { Bitboards[9] = value; }
         }
         ulong BlackQueens
         {
-            get { return bitboards[10]; }
-            set { bitboards[10] = value; }
+            get { return Bitboards[10]; }
+            set { Bitboards[10] = value; }
         }
         ulong BlackKing
         {
-            get { return bitboards[11]; }
-            set { bitboards[11] = value; }
+            get { return Bitboards[11]; }
+            set { Bitboards[11] = value; }
         }
         #endregion
 
@@ -85,15 +90,16 @@ namespace KokosEngine
         bool CastleRightBlackShort;
         bool CastleRightBlackLong;
 
-        Piece[] mailbox;
+        Piece[] Mailbox;
+
         internal Board()
         {
-            mailbox = new Piece[64];
-            bitboards = new ulong[12];
+            Mailbox = new Piece[64];
+            Bitboards = new ulong[12];
         }
         internal void MakeMove(Move move)
         {
-            //todo castling rights, move count
+            //todo move count
 
             DisableEnPassant();
 
@@ -111,57 +117,74 @@ namespace KokosEngine
                     SetSquareTo(move);
                     break;
             }
-            SwitchSideToMove();
+            SwitchPlayerToMove();
         }
-        internal void DisableEnPassant()
+        internal void MakeKingCastle()
         {
-            possibleEPSquare = 0;
-        }
-        internal void SwitchSideToMove()
-        {
-            isWhiteToMove = !isWhiteToMove;
-        }
-        internal void SetSquareTo(Move move)
-        {
-            //todo figure out a way to quickly get bitboard from IsWhiteToMove and move.piece
-
-            if (!move.IsPromotion)
+            if (WhiteMove)
             {
-
+                ClearSquare(4);
+                SetPieceOnSquare(WK, 6);
+                ClearSquare(7);
+                SetPieceOnSquare(WR, 5);
             }
             else
             {
-                MoveType t = move.Type;
-                if (t == MoveType.PromoQueen || t == MoveType.PromoCaptureQueen)
-                {
-
-                }
-                if (t == MoveType.PromoKnight || t == MoveType.PromoCaptureKnight)
-                {
-
-                }
-                if (t == MoveType.PromoBishop || t == MoveType.PromoCaptureBishop)
-                {
-
-                }
-                if (t == MoveType.PromoRook || t == MoveType.PromoCaptureRook)
-                {
-
-                }
+                ClearSquare(60);
+                SetPieceOnSquare(WK, 62);
+                ClearSquare(63);
+                SetPieceOnSquare(WR, 61);
             }
         }
-        internal void SetBit(ref ulong bitboard, int index)
+        internal void MakeQueenCastle()
+        {
+            if (WhiteMove)
+            {
+                ClearSquare(4);
+                SetPieceOnSquare(WK, 2);
+                ClearSquare(0);
+                SetPieceOnSquare(WR, 3);
+            }
+            else
+            {
+                ClearSquare(60);
+                SetPieceOnSquare(WK, 58);
+                ClearSquare(56);
+                SetPieceOnSquare(WR, 59);
+            }
+        }
+        internal void DisableEnPassant()
+        {
+            PossibleEPSquare = 0;
+        }
+        internal void SwitchPlayerToMove()
+        {
+            PlayerToMove = -PlayerToMove;
+        }
+        internal void SetSquareTo(Move move)
+        {
+            if (move.IsPromotion)
+            {
+                //decipher this i dare you
+                int index = 4 + move.Special + (-3 * PlayerToMove);
+                SetPieceOnSquare((Piece)index, move.SquareTo);
+            }
+            else
+            {
+                SetPieceOnSquare(move.PieceMoved, move.SquareTo);
+            }
+        }
+        internal void SetPieceOnSquare(Piece piece, int index)
         {
             ulong mask = (ulong)1 << index;
-            bitboard |= mask;
+            Bitboards[(int)piece] |= mask;
+            Mailbox[index] = piece;
         }
         internal void ClearCapturedPiece(Move move)
         {
             if (move.Type == MoveType.EnPassant)
             {
-                int offset;
-                if (isWhiteToMove) offset = -8;
-                else offset = 8;
+                int offset = -8 * PlayerToMove;
                 ClearSquare(move.SquareTo + offset);
             }
             else
@@ -171,25 +194,25 @@ namespace KokosEngine
         }
         internal void ClearSquare(int square)
         {
-            mailbox[square] = X;
+            Mailbox[square] = X;
             ClearBitInAllBitboards(square);
         }
         internal void ClearBitInAllBitboards(int square)
         {
             ulong mask = ~((ulong)1 << square);
-            for (int i = 0; i < bitboards.Length; i++)
+            for (int i = 0; i < Bitboards.Length; i++)
             {
-                bitboards[i] &= mask;
+                Bitboards[i] &= mask;
             }
         }
         internal void SetStartingPosition()
         {
-            isWhiteToMove = true;
+            WhiteMove = true;
 
-            halfmovesReversible = 0;
-            fullmovesTotal = 0;
+            HalfmovesReversible = 0;
+            FullmovesTotal = 0;
 
-            possibleEPSquare = 0x0000000000000000;
+            PossibleEPSquare = 0x0000000000000000;
 
             WhitePawns = 0x000000000000FF00;
             WhiteKnights = 0x0000000000000042;
@@ -209,7 +232,7 @@ namespace KokosEngine
             CastleRightBlackShort = true;
             CastleRightBlackLong = true;
 
-            mailbox = new Piece[]
+            Mailbox = new Piece[]
             {
                 WR, WN, WB, WQ, WK, WB, WN, WR,
                 WP, WP, WP, WP, WP, WP, WP, WP,
@@ -233,7 +256,7 @@ namespace KokosEngine
             {
                 for (int file = 0; file < 8; file++)
                 {
-                    p = mailbox[8 * rank + file];
+                    p = Mailbox[8 * rank + file];
                     if (p == X)
                     {
                         emptyCounter++;
@@ -262,7 +285,7 @@ namespace KokosEngine
 
             sb.Append(' '); //section: active color
 
-            if (isWhiteToMove) sb.Append('w');
+            if (WhiteMove) sb.Append('w');
             else sb.Append('b');
 
             sb.Append(' '); //section: castling
@@ -281,15 +304,15 @@ namespace KokosEngine
 
             sb.Append(' '); //section: en passant
 
-            sb.Append(BBUtil.LSB1ToAlgebraic(possibleEPSquare));
+            sb.Append(BBUtil.LSB1ToAlgebraic(PossibleEPSquare));
 
             sb.Append(' '); //section: halfmove clock
 
-            sb.Append(halfmovesReversible.ToString());
+            sb.Append(HalfmovesReversible.ToString());
 
             sb.Append(' '); //section: fullmove number
 
-            sb.Append(fullmovesTotal.ToString());
+            sb.Append(FullmovesTotal.ToString());
 
             return sb.ToString();
         }
